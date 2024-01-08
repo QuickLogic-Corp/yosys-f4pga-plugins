f28967105f83f1c28432309407d1ccfd1a064eee
rakeshm <rakeshm@quicklogic.com>
2024-01-08 00:54:47 -0800

    modified the ff map to support dffre & dffnre instead of sh_dff for the shift register designs

--- ql-qlf-plugin/qlf_k6n10f/ffs_map.v
+++ ql-qlf-plugin/qlf_k6n10f/ffs_map.v
@@ -125,26 +125,79 @@ module \$__SHREG_DFF_P_ (D, Q, C);
 
         // First in chain
         generate if (i == 0) begin
-                 sh_dff #() shreg_beg (
+                 dffre #() shreg_beg (
                     .Q(q[i]),
                     .D(D),
-                    .C(C)
+                    .C(C),
+					.E(1'b1),
+					.R(1'b1)
                 );
         end endgenerate
         // Middle in chain
         generate if (i > 0 && i != DEPTH-1) begin
-                 sh_dff #() shreg_mid (
+                 dffre #() shreg_mid (
                     .Q(q[i]),
                     .D(q[i-1]),
-                    .C(C)
+                    .C(C),
+					.E(1'b1),
+					.R(1'b1)
                 );
         end endgenerate
         // Last in chain
         generate if (i == DEPTH-1) begin
-                 sh_dff #() shreg_end (
+                 dffre #() shreg_end (
                     .Q(Q),
                     .D(q[i-1]),
-                    .C(C)
+                    .C(C),
+					.E(1'b1),
+					.R(1'b1)
+                );
+        end endgenerate
+   end: slice
+   endgenerate
+
+endmodule
+
+module \$__SHREG_DFF_N_ (D, Q, C);
+    input  D;
+    input  C;
+    output Q;
+
+    parameter DEPTH = 2;
+
+    reg [DEPTH-2:0] q;
+
+    genvar i;
+    generate for (i = 0; i < DEPTH; i = i + 1) begin: slice
+
+        // First in chain
+        generate if (i == 0) begin
+                 dffnre #() shreg_beg (
+                    .Q(q[i]),
+                    .D(D),
+                    .C(C),
+					.E(1'b1),
+					.R(1'b1)
+                );
+        end endgenerate
+        // Middle in chain
+        generate if (i > 0 && i != DEPTH-1) begin
+                 dffnre #() shreg_mid (
+                    .Q(q[i]),
+                    .D(q[i-1]),
+                    .C(C),
+					.E(1'b1),
+					.R(1'b1)
+                );
+        end endgenerate
+        // Last in chain
+        generate if (i == DEPTH-1) begin
+                 dffnre #() shreg_end (
+                    .Q(Q),
+                    .D(q[i-1]),
+                    .C(C),
+					.E(1'b1),
+					.R(1'b1)
                 );
         end endgenerate
    end: slice
