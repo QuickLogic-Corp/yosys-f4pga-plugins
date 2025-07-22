17395d13dfca1d58f26aa1606247acb72ab01f82
Oleksandr <apivovarov@quicklogic.com>
2025-07-22 17:23:09 +0300

    update synth_quicklogic.cc with one given by Sara

--- ql-qlf-plugin/synth_quicklogic.cc
+++ ql-qlf-plugin/synth_quicklogic.cc
@@ -285,6 +285,10 @@ struct SynthQuickLogicPass : public ScriptPass {
         if (family != "pp3" && family != "qlf_k4n8" && family != "qlf_k6n10" && family != "qlf_k6n10f")
             log_cmd_error("Invalid family specified: '%s'\n", family.c_str());
 
+        // if (family != "pp3") {
+        //     abc9 = false;
+        // }
+
         if (family == "qlf_k4n8") {
             nosdff = true;
         }
@@ -330,32 +334,23 @@ struct SynthQuickLogicPass : public ScriptPass {
                 }
                 if (synplify) {
                     readVelArgs += family_path + "/synplify_map.v";
-					readVelArgs += family_path + "/synplify_bram_map.v";
+					readVelArgs += family_path + "/synplify_dsp_map.v";
                 }
             }
+
             // Use -nomem2reg here to prevent Yosys from complaining about
             // some block ram cell models. After all the only part of the cells
             // library required here is cell port definitions plus specify blocks.
             run("read_verilog -lib -specify -nomem2reg " + readVelArgs);
-			if (synplify) {
-			    run("read_verilog " + family_path + "/QL_DSPV2.v");
-			}
             run(stringf("hierarchy -check %s", help_mode ? "-top <top>" : top_opt.c_str()));
         }
 
         if (check_label("prepare")) {
             if (synplify) {
-				run("proc");
-				run("flatten");
-				run("opt -nodffe -nosdff");
-				run("fsm");
-				run("wreduce");
-				run("peepopt");
-				run("opt_clean");
-				run("share");
+                run("flatten");
             }
             else{
-			    run("proc");
+                run("proc");
                 run("flatten");
                 if (help_mode || family == "pp3") {
                     run("tribuf -logic", "                   (for pp3)");
@@ -409,7 +404,7 @@ struct SynthQuickLogicPass : public ScriptPass {
                     run("chtype -set $mul t:$__soft_mul", "  (for qlf_k6n10)");
                 }
             }
-            if (help_mode || family == "qlf_k6n10f") {			
+            if (help_mode || family == "qlf_k6n10f") {
 
                 struct DspParams {
                     size_t a_maxwidth;
@@ -462,7 +457,7 @@ struct SynthQuickLogicPass : public ScriptPass {
         }
 
         if (check_label("coarse")) {
-            //if (!synplify) {
+            if (!synplify) {
                 run("techmap -map +/cmp2lut.v -D LUT_WIDTH=4");
                 if (!noOpt) {
                     run("opt_expr");
@@ -477,15 +472,12 @@ struct SynthQuickLogicPass : public ScriptPass {
                 if (!noOpt) {
                     run("opt_clean");
                 }
-            //}
+            }
         }
 
         if (check_label("map_bram", "(skip if -no_bram)") && (help_mode || family == "qlf_k6n10" || family == "qlf_k6n10f" || family == "pp3") &&
             inferBram) {
             if (help_mode || family == "qlf_k6n10f") {
-				if (synplify) {
-					run("techmap -autoproc -map " + lib_path + family + "/synplify_bram_map.v");
-				}
                 if (notdpram) {
                     run("memory_libmap -lib " + lib_path + family + "/libmap_brams_sdp.txt", "(for qlf_k6n10f)");
                     run("ql_sdpbram_merge", "(for qlf_k6n10f)");
@@ -531,7 +523,7 @@ struct SynthQuickLogicPass : public ScriptPass {
         }
 
         if (check_label("map_gates")) {
-            //if (!synplify) {
+            if (!synplify) {
                 if (help_mode || (inferAdder && (family == "qlf_k4n8" || family == "qlf_k6n10" || family == "qlf_k6n10f"))) {
                     run("techmap -map +/techmap.v -map " + lib_path + family + "/arith_map.v", "(unless -no_adder)");
                 } else {
@@ -549,11 +541,11 @@ struct SynthQuickLogicPass : public ScriptPass {
                     run("opt_clean");
                     run("opt" + noDFFArgs);
                 }
-            //}
+            }
         }
 
         if (check_label("map_ffs")) {
-            //if (!synplify) {
+            if (!synplify) {
                 if (!noOpt) {
                     run("opt_expr");
                 }
@@ -576,7 +568,7 @@ struct SynthQuickLogicPass : public ScriptPass {
                     } else if (mince_num != "") {
                         legalizeArgs = " -mince " + mince_num + " -cell $_DFFE_?N?P_ 0 -cell $_DFF_?N?_ 0"; 
                     } else {
-						legalizeArgs = " -cell $_DFFE_?N?P_ 0";
+						legalizeArgs = " -cell $_DFFE_?N?P_ 0 -cell $_DFFE_?P_ 0 -cell $_DFF_?N?_ 0 -cell $_DFF_?_ 0";
 					}
                     if (!nosdff) {
 						if (noffenable) {
@@ -584,7 +576,7 @@ struct SynthQuickLogicPass : public ScriptPass {
 						} else if (mince_num != "") {
 							legalizeArgs += " -mince " + mince_num + " -cell $_SDFFE_?N?P_ 0 -cell $_SDFF_?N?_ 0";
 						} else {
-							legalizeArgs += " -cell $_SDFFE_?N?P_ 0";							
+							legalizeArgs += "  -cell $_SDFFE_?N?P_ 0 -cell $_DFFE_?P_ 0 -cell $_SDFF_?N?_ 0 -cell $_DFF_?_ 0";							
 						}					
                     }
                     run("dfflegalize" + legalizeArgs);
@@ -593,6 +585,12 @@ struct SynthQuickLogicPass : public ScriptPass {
                     run("techmap -map " + lib_path + family + "/cells_map.v");
                 }
 				std::string techMapArgs = " -map +/techmap.v -map " + lib_path + family + "/ffs_map.v";
+                //std::string techMapArgs;
+                //if (nosetff) {
+                //    techMapArgs = " -map +/techmap.v -map " + lib_path + family + "/ffs_map_noaset.v";
+                //} else {
+                //    techMapArgs = " -map +/techmap.v -map " + lib_path + family + "/ffs_map.v";
+                //}
                 if (help_mode || !noffmap) {
                     run("techmap " + techMapArgs, "(unless -no_ff_map)");
                 }
@@ -604,11 +602,11 @@ struct SynthQuickLogicPass : public ScriptPass {
                     run("opt_clean");
                     run("opt" + noDFFArgs);
                 }
-            //}
+            }
         }
 
         if (check_label("map_luts")) {
-            //if (!synplify) {
+            if (!synplify) {
                 if (help_mode || abcOpt) {
                     if (help_mode || family == "qlf_k6n10" || family == "qlf_k6n10f") {
                         if (abc9) {
@@ -651,7 +649,7 @@ struct SynthQuickLogicPass : public ScriptPass {
                 if (!noOpt) {
                     run("opt_lut");
                 }
-            //}
+            }
         }
 
         if (check_label("map_cells", "(for pp3, qlf_k6n10)") && (help_mode || family == "qlf_k6n10" || family == "pp3")) {
@@ -706,9 +704,12 @@ struct SynthQuickLogicPass : public ScriptPass {
             std::string family_path = " " + lib_path + family;
             if (family == "qlf_k6n10f") {
                 if (synplify) {
-					run("opt -fast -mux_undef -undriven -fine" + noDFFArgs);
+                    run("opt -fast -mux_undef -undriven -fine" + noDFFArgs);
                     run("techmap -autoproc -map" + family_path + "/synplify_map.v");
-					run("opt" + noDFFArgs);
+                    run("opt_expr");
+                    run("opt_merge");
+                    run("opt_clean -purge");
+					run("techmap -autoproc -map" + family_path + "/synplify_dsp_map.v");			
                     run("opt_expr");
                     run("opt_merge");
                     run("opt_clean -purge");
